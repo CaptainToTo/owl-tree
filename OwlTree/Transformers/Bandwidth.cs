67bbab1ad04e77b122c4b80faff481bb79d108ea
atumemot <atumemot@gmail.com>
2025-05-04 14:42:35 -0700

    added MB to bandwidth recording

--- OwlTree/Transformers/Bandwidth.cs
+++ OwlTree/Transformers/Bandwidth.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace OwlTree
 {
+    /// <summary>
+    /// Records packet sizes to produce stats on bandwidth usage.
+    /// </summary>
     public class Bandwidth
     {
 
@@ -12,30 +15,54 @@ namespace OwlTree
         private int _max;
         private Action<Bandwidth> _report;
 
+        private int _totalSent = 0;
+        private int _totalRecv = 0;
+        private long _startTime = 0;
+
+        /// <summary>
+        /// Provide a callback for anytime a new packet report is made.
+        /// </summary>
         public Bandwidth(Action<Bandwidth> report, int max = 30)
         {
             _max = max;
             _report = report;
+            _startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
         }
 
+        /// <summary>
+        /// Record the size of the given packet as part of outgoing data.
+        /// </summary>
         public void RecordOutgoing(Packet packet)
         {
-            _outgoingRecords.Add((packet.GetPacket().Length, DateTimeOffset.Now.ToUnixTimeMilliseconds()));
+            var size = packet.GetPacket().Length;
+            _outgoingRecords.Add((size, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()));
+            _totalSent += size;
             if (_outgoingRecords.Count > _max)
                 _outgoingRecords.RemoveAt(0);
             _report.Invoke(this);
         }
 
+        /// <summary>
+        /// The size and timestamp of the last outgoing packet recorded.
+        /// </summary>
         public (int bytes, long time) LastOutgoing() => _outgoingRecords.Count == 0 ? (0, 0) : _outgoingRecords.Last();
 
+        /// <summary>
+        /// Record the size of the given packet as part of incoming data.
+        /// </summary>
         public void RecordIncoming(Packet packet)
         {
-            _incomingRecords.Add((packet.GetPacket().Length, DateTimeOffset.Now.ToUnixTimeMilliseconds()));
+            var size = packet.GetPacket().Length;
+            _incomingRecords.Add((size, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()));
+            _totalRecv += size;
             if (_incomingRecords.Count > _max)
                 _incomingRecords.RemoveAt(0);
             _report.Invoke(this);
         }
 
+        /// <summary>
+        /// The size and timestamp of the last incoming packet recorded.
+        /// </summary>
         public (int bytes, long time) LastIncoming() => _incomingRecords.Count == 0 ? (0, 0) : _incomingRecords.Last();
 
         public float OutgoingBytesPerSecond()
@@ -45,13 +72,10 @@ namespace OwlTree
             int sum = 0;
             for (int i = 0; i < _outgoingRecords.Count; i++)
                 sum += _outgoingRecords[i].bytes;
-            return sum / ((DateTimeOffset.Now.ToUnixTimeMilliseconds() - _outgoingRecords[0].time) / 1000f);
+            return sum / ((DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _outgoingRecords[0].time) / 1000f);
         }
 
-        public float OutgoingKbPerSecond()
-        {
-            return OutgoingBytesPerSecond() / 1000f;
-        }
+        public float OutgoingKbPerSecond() => OutgoingBytesPerSecond() / 1000f;
 
         public float IncomingBytesPerSecond()
         {
@@ -60,12 +84,17 @@ namespace OwlTree
             int sum = 0;
             for (int i = 0; i < _incomingRecords.Count; i++)
                 sum += _incomingRecords[i].bytes;
-            return sum / ((DateTimeOffset.Now.ToUnixTimeMilliseconds() - _incomingRecords[0].time) / 1000f);
+            return sum / ((DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _incomingRecords[0].time) / 1000f);
         }
 
-        public float IncomingKbPerSecond()
-        {
-            return IncomingBytesPerSecond() / 1000f;
-        }
+        public float IncomingKbPerSecond() => IncomingBytesPerSecond() / 1000f;
+
+        public float TotalMbSent() => _totalSent / 1000000f;
+
+        public float OutgoingMbPerHour() => TotalMbSent() / ((DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _startTime) / 3600000f);
+
+        public float TotalMbRecv() => _totalRecv / 1000000f;
+
+        public float IncomingMbPerHour() => TotalMbRecv() / ((DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _startTime) / 3600000f);
     }
 }
